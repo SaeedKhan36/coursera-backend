0a959f447b2d0047ff4e854a4eab955fc07d1bc0
SaeedKhan36 <saeedkhan364.com@gmail.com>
2025-11-23 18:18:26 +0530

    sign up sign in updated

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const userRouters = express.Router();
 const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "asdasdasd";
+
 
 userRouters.post("/signup", async function (req, res) {
 const {email,password,firstName,lastName}= req.body
@@ -25,6 +28,17 @@ userRouters.post("/signin",  async function (req, res) {
     email:email,
     password:password
   })
+  if (user){
+    jwt.sign({
+      id:user._id
+    }, SECRET_KEY)
+    
+  }
+  else{
+    res.status(401).json({
+      message: "Invalid credentials"
+    });
+  }
 
 
   res.json({
